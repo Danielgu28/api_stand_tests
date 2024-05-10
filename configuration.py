f2aa2cdeaca2aebc50e6a50df3139b7b47087511
Danielgu28 <guerreroesteban.2028@gmail.com>
2024-05-09 21:46:49 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-977d3125-c59e-4118-9ee7-61f5a65674ac.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
